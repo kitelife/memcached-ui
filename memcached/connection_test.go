792648ebd266582d76f6954360004c1ae6c28a4a
youngsterxyf <sas.198708@gmail.com>
2015-10-13 00:07:01 +0800

    test for connection.go

---
+++ memcached/connection_test.go
@@ -0,0 +1,20 @@
+package memcached
+
+import (
+	"testing"
+)
+
+func Test_Send(t *testing.T) {
+	c := Connection{
+		Host: "127.0.0.1",
+		Port: 11211,
+	}
+	cmd := "stats\r\n"
+	resp, err := c.Send(cmd)
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(string(resp))
+	}
+	c.Conn.Close()
+}
